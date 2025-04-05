985df5903298a2b6ac736525a64775dced2badf7
Tosini13 <jbartos13@gmail.com>
2025-04-05 23:03:53 +0200

    chore: fix volume in fly

--- config/database.ts
+++ config/database.ts
@@ -44,7 +44,7 @@ export default ({ env }) => {
     },
     sqlite: {
       connection: {
-        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        filename: env('DATABASE_FILENAME', '/strapi_data/strapi.db'),
       },
       useNullAsDefault: true,
     },
