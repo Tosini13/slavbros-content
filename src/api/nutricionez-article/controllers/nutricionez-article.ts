3d82ea1c9a9418e6e617bf9f3f28257011e3fd79
Tosini13 <jbartos13@gmail.com>
2025-04-06 17:59:54 +0200

    feat: add nutricionez article and service

---
+++ src/api/nutricionez-article/controllers/nutricionez-article.ts
@@ -0,0 +1,7 @@
+/**
+ * nutricionez-article controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::nutricionez-article.nutricionez-article');
---
+++ src/api/nutricionez-service/routes/nutricionez-service.ts
@@ -0,0 +1,7 @@
+/**
+ * nutricionez-service router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::nutricionez-service.nutricionez-service');
