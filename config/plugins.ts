4c3ec3b66710fd11359d5840f73e68f2ad64637f
Tosini13 <jbartos13@gmail.com>
2025-04-22 16:22:18 +0200

    chore: install nodemailer

--- config/plugins.ts
+++ config/plugins.ts
@@ -25,4 +25,21 @@ export default ({env}) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env('SMTP_PORT'),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: env('SMTP_DEFAULT_FROM'),
+        defaultReplyTo: env('SMTP_DEFAULT_REPLY_TO'),
+      },
+    },
+  },
 });
